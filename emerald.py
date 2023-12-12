bb598765bbf8f845a587b5321884106dacd7d905
luis-pvcc <146022441+luis-pvcc@users.noreply.github.com>
2023-12-12 00:38:11 -0500

    Add files via upload

--- emerald.py
+++ emerald.py
@@ -55,10 +55,11 @@ def open_out_file():
     global f
     f = open(outfile, 'w')
     f.write('<html> <head> <title> Emerald Beach Hotel & Resort </title>\n')
-    f.write('<style>td { text-align: right }.center { text-align: center; } body {font-family: Arial, sans-serif;background-image: url(hotel2.jpg)}table, th, td {border: 1.5px solid black;border-collapse: collapse;}</style>\n')
+    f.write('<style> tr:hover {background-color: #7db47f;} tr {border-bottom: 1px solid #ddd;}td { text-align:center }.center { text-align: center; } body {font-family: Arial, sans-serif;background-image: url(hotel2.jpg)}table, th, td {border: 1.5px solid black;border-collapse: collapse;}</style>\n')
     f.write('</head><body style="font-family: Arial, sans-serif;">\n')
-    f.write('<table border="2" style="color: #ffffff; background-color:#006d00; margin: auto; width: 50%;">\n')
+    f.write('<table border="2" style="color: #ffffff; background-color:#006d00; margin: auto; width: 40%;">\n')
     f.write('<tr> <td colspan="7" class="center"><h2>Emerald Beach Hotel & Resort</h2>\n')
+    f.write('<table border="2" style="color: #ffffff; background-color:#999999; margin: auto; width: 70%;">\n')
     f.write('<tr><th>Name</th><th>Room Type</th><th>Number of Nights</th><th>Subtotal</th><th>Sales Tax</th><th>Occupancy Tax</th><th>Total</th></tr>\n')
 
 def create_output_html():
